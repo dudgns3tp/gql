b00324f15325b4cef3f44bf2299ad3895186cd57
younghoon.choi <c_yh0327@naver.com>
2021-02-25 11:39:10 +0900

    init real

--- src/index.js
+++ src/index.js
@@ -1,8 +1,5 @@
 import { ApolloServer, gql } from 'apollo-server';
 
-// A schema is a collection of type definitions (hence "typeDefs")
-// that together define the "shape" of queries that are executed against
-// your data.
 const books = [
   {
     title: 'The Awakening',
@@ -25,19 +22,14 @@ const typeDefs = gql`
   }
 `;
 
-// Resolvers define the technique for fetching the types defined in the
-// schema. This resolver retrieves books from the "books" array above.
 const resolvers = {
   Query: {
     books: () => books,
   },
 };
 
-// The ApolloServer constructor requires two parameters: your schema
-// definition and your set of resolvers.
 const server = new ApolloServer({ typeDefs, resolvers });
 
-// The `listen` method launches a web server.
 server.listen().then(({ url }) => {
   console.log(`🚀  Server ready at ${url}`);
 });
