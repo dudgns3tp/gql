db9ce0b3be899ea0646ecf7faeae324741e6df2e
younghoon.choi <c_yh0327@naver.com>
2021-02-25 11:23:07 +0900

    init 끝

--- src/index.js
+++ src/index.js
@@ -1,23 +1,43 @@
-const express = require('express');
-const { ApolloServer, gql } = require('apollo-server-express');
- 
+import { ApolloServer, gql } from 'apollo-server';
+
+// A schema is a collection of type definitions (hence "typeDefs")
+// that together define the "shape" of queries that are executed against
+// your data.
+const books = [
+  {
+    title: 'The Awakening',
+    author: 'Kate Chopin',
+  },
+  {
+    title: 'City of Glass',
+    author: 'Paul Auster',
+  },
+];
+
 const typeDefs = gql`
+  type Book {
+    title: String
+    author: String
+  }
+
   type Query {
-    hello: String
+    books: [Book]
   }
 `;
- 
+
+// Resolvers define the technique for fetching the types defined in the
+// schema. This resolver retrieves books from the "books" array above.
 const resolvers = {
   Query: {
-    hello: () => 'Hello world!',
+    books: () => books,
   },
 };
- 
+
+// The ApolloServer constructor requires two parameters: your schema
+// definition and your set of resolvers.
 const server = new ApolloServer({ typeDefs, resolvers });
- 
-const app = express();
-server.applyMiddleware({ app });
- 
-app.listen({ port: 4000 }, () =>
-  console.log('Now browse to http://localhost:4000' + server.graphqlPath)
-);
+
+// The `listen` method launches a web server.
+server.listen().then(({ url }) => {
+  console.log(`🚀  Server ready at ${url}`);
+});
