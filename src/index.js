797f90724439a8d68431167671a3d46612afe090
younghoon.choi <c_yh0327@naver.com>
2021-02-26 11:15:59 +0900

    [chore] 오타 수정

--- src/index.js
+++ src/index.js
@@ -2,13 +2,13 @@ import { ApolloServer } from 'apollo-server';
 import queries from './graphql/schema/_queries.js';
 import mutation from './graphql/schema/_mutations.js';
 import boardTypeDefs from './graphql/schema/board.js';
-import boardReslovers from './graphql/resolver/board.js';
+import boardResolvers from './graphql/resolver/board.js';
 import userResolvers from './graphql/resolver/user.js';
 import userTypeDefs from './graphql/schema/user.js';
 
 const typeDefs = [queries, mutation, boardTypeDefs, userTypeDefs];
 
-const resolvers = [boardReslovers, userResolvers];
+const resolvers = [boardResolvers, userResolvers];
 
 const server = new ApolloServer({ typeDefs, resolvers });
 
