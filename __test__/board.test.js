9803d207f2affabc073b4109cf2c91dc29568f72
younghoon.choi <c_yh0327@naver.com>
2021-03-04 11:11:51 +0900

    [test] create test

--- __test__/board.test.js
+++ __test__/board.test.js
@@ -23,3 +23,33 @@ test('get board', async () => {
 
     expect(getBoard).toEqual({ _id: '603df6147047418c15bfe2d7', author: '최영훈' });
 });
+
+test('create new Board', async () => {
+    const CREATE_NEW_BOARD = gql`
+        mutation($title: String!, $author: String!, $content: String!, $label: [label]) {
+            addBoard(title: $title, author: $author, content: $content, label: $label) {
+                _id
+                title
+                content
+                author
+                label
+                createdAt
+                updatedAt
+                seq
+                like
+            }
+        }
+    `;
+    const {
+        data: { addBoard },
+    } = await mutate({
+        mutation: CREATE_NEW_BOARD,
+        variables: {
+            title: '테스트 코드로 작성한 게시글 입니다.',
+            author: '최영훈',
+            content: '테스트중',
+            label: ['bug'],
+        },
+    });
+    expect(addBoard).toBeTruthy();
+});
